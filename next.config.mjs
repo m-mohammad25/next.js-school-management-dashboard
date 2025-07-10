a301c92c94bb469df4cdaefb08520437ace20012
Mohammad Mohammad <m.mohammad25@outlook.com>
2025-07-10 16:03:59 +0300

    added the redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
+  redirects: async () => [
+    // I have added this temporarly to redirect to the teachers page
+    { source: "/", destination: "/list/teachers", permanent: true },
+  ],
 };
 
 export default nextConfig;
