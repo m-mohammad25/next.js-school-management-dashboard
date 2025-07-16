6f3fbe991aea3960390aa5cf18501c70f5f1bfb5
Mohammad Mohammad <m.mohammad25@outlook.com>
2025-07-16 14:12:08 +0300

    started to integrating Clerk and finishing login page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Navbar() {
@@ -27,13 +28,7 @@ export default function Navbar() {
           <span className="text-sm leading-3 font-medium">John Doe</span>
           <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
-        <Image
-          src="/avatar.png"
-          alt="avatar"
-          width={36}
-          height={36}
-          className="rounded-full"
-        />
+        <UserButton />
       </div>
     </div>
   );
