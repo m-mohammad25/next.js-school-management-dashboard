6d5c539f3608ca67201db1ef7aeb4002df348525
Mohammad Mohammad <m.mohammad25@outlook.com>
2025-11-06 17:32:42 +0300

    removed "name" property from the lesson seeding function

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -80,7 +80,6 @@ async function main() {
   for (let i = 1; i <= 30; i++) {
     await prisma.lesson.create({
       data: {
-        name: `Lesson${i}`,
         day: Day[
           Object.keys(Day)[
             Math.floor(Math.random() * Object.keys(Day).length)
